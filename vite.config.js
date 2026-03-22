cdd05b1ccf4232a4ca8b54597a1664857fd1d320
Ponkarthick <ponkarthickramu16@gmail.com>
2026-03-22 19:12:22 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@livekit/components-react',
+      '@livekit/components-core',
+      'livekit-client',
+    ],
+    force: true,
+  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          livekit: ['@livekit/components-react', 'livekit-client'],
+          firebase: ['firebase/app', 'firebase/firestore', 'firebase/auth'],
+        },
+      },
+    },
+  },
 })
