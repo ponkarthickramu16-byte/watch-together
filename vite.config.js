3883f774d300010740972f6d3f161063c3774775
Ponkarthick <ponkarthickramu16@gmail.com>
2026-03-23 13:20:24 +0530

    enable sourcemap debug

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      '@livekit/components-react',
-      '@livekit/components-core',
-      'livekit-client',
-    ],
-  },
   build: {
+    sourcemap: true,
     chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('livekit')) return 'livekit';
-          if (id.includes('firebase')) return 'firebase';
-          if (id.includes('node_modules')) return 'vendor';
-        },
-      },
-    },
   },
 })
